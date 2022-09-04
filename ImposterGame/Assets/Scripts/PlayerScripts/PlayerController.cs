2877b96096d094667d2c7ddef18dd1190d5aadf7
ForNoPurpose <109082583+ForNoPurpose@users.noreply.github.com>
2022-09-04 17:05:13 -0500

    Revert "My god, idk what I'm doing."
    This reverts commit 786a6c6138d5d52e4af45a60836e443eb5bf8bcd.

--- ImposterGame/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ ImposterGame/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Pool;
 
 public class PlayerController : MonoBehaviour
 {
     public HealthSystem playerHealth;
-    public uint playerBufferMaxSize;
-    public uint playerBufferCurrentSize = 0;
+    public float playerBuffer;
     [SerializeField] private uint playerLevel = 0;
-    //[SerializeField] private uint playerExperience = 0;
-
-    public List<IWeapon> playerBuffer = new();
+    [SerializeField] private uint playerExperience = 0;
 
     private void Awake()
     {
         playerHealth.health = 100 + playerLevel * 10;
-        playerBufferMaxSize = 10 + playerLevel;
+        playerBuffer = 10 + playerLevel;
     }
-    
+
 }
