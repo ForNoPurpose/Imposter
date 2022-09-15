bdcc752077a359abb46cfc99ceb619b444a52a73
ForNoPurpose <109082583+ForNoPurpose@users.noreply.github.com>
2022-09-14 23:07:37 -0500

    almost done with dialogue system

--- ImposterGame/Assets/Scripts/DialogueSystem/DialogueTrigger.cs
+++ ImposterGame/Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 using UnityEngine.InputSystem;
+using System.Text;
 
 public sealed class DialogueTrigger : MonoBehaviour
 {
@@ -18,32 +19,60 @@ public sealed class DialogueTrigger : MonoBehaviour
     private bool dialogActive = false;
     private WaitForSeconds _speachDelay = new(0.05f);
 
+    private Collider2D _targetCollider;
+
+    [SerializeField] private Vector3 _offset = new Vector3(1.3f, 1f, 0);
+    private Vector3 _startPos;
+    private float _deactivateRange = 5f;
+
+    [SerializeField] private bool _repeatDialogue = false;
+    private bool _visited = false;
+
     private void Start()
     {
         _canvas = GetComponentInChildren<Canvas>(true);
+        _startPos = transform.position;
     }
 
     private void Update()
     {
-        if (dialogActive && Keyboard.current.fKey.wasPressedThisFrame)
+
+        if (dialogActive)
         {
-            StopAllCoroutines();
-            DisplayNextSentence();
+            _canvas.transform.position = _targetCollider.transform.position + _offset;
+            if (Keyboard.current.fKey.wasPressedThisFrame)
+            {
+                StopAllCoroutines();
+                DisplayNextSentence(); 
+            }
+
+            if(Vector3.Distance(_canvas.transform.position, _startPos) > _deactivateRange)
+            {
+                dialogActive = false;
+                _canvas.gameObject.SetActive(false);
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        dialogActive = true;
-        _canvas.gameObject.SetActive(true);
-        StartDialogue();
+
+        if (!_visited)
+        {
+            _targetCollider = collision;
+            dialogActive = true;
+            _canvas.gameObject.SetActive(true);
+            StartDialogue(); 
+        }
+        if(!_repeatDialogue) _visited = true;
+        else _visited = false;
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        dialogActive = false;
-        _canvas.gameObject.SetActive(false);
-    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    dialogActive = false;
+    //    _canvas.gameObject.SetActive(false);
+    //}
 
     public void StartDialogue()
     {
@@ -72,10 +101,10 @@ public sealed class DialogueTrigger : MonoBehaviour
 
     IEnumerator TypeWriter(string line)
     {
-        DialogueText.text = "";
-        foreach (var letter in line)
+        DialogueText.text = line;
+        for (int i = 1; i <= line.Length; i++)
         {
-            DialogueText.text += letter;
+            DialogueText.maxVisibleCharacters = i;
             yield return _speachDelay;
         }
     }
